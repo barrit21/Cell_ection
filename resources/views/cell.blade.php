124bf54f3b1b4cc1080feaa0724855a9499e71b9
MrHaar <47451387+MrHaar@users.noreply.github.com>
2019-04-01 15:35:06 +0200

    Simple ajout de flèches sur les menus

--- resources/views/cell.blade.php
+++ resources/views/cell.blade.php
@@ -1,5 +1,7 @@
 @section('content')
 
+<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
+
 <?php
 $nbreplicate=count($data);
 
@@ -40,19 +42,19 @@ var $table = $('#table');
           $(this).toggleClass("bold-blue");
   });
 
-function linkFormatterGene(value) 
+function linkFormatterGene(value)
 {
     return '<a href="/gene/'+value+'">'+value+'</a>';
 }
-function linkFormatterGeneset(value) 
+function linkFormatterGeneset(value)
 {
     return '<a href="/geneset/'+value+'">'+value+'</a>';
 }
- function imgFormatter(value) 
+ function imgFormatter(value)
  {
     return '<img src="/images/'+value+'.png"/>';
 }
-  function commFormatter(value) 
+  function commFormatter(value)
 {
     value=value.replace("&amp;", "&");
     return value;
@@ -65,7 +67,7 @@ function linkFormatterGeneset(value)
       </li>
       <li>
         <a data-toggle="tab" href="#MenuGenes">Expression level</a>
-      </li>      
+      </li>
       <li>
         <a data-toggle="tab" href="#MenuGSEA">GSEA</a>
       </li>
@@ -82,28 +84,29 @@ function linkFormatterGeneset(value)
          </blockquote>
 
           <h4><?= $cell->name; ?> has <?= count($data); ?> replicates in our data for this meta-analysis.</h4>
-          
-          <table id="table" 
+
+          <table id="table"
             data-toggle="table"
             data-search="true"
-            data-filter-control="true" 
+            data-filter-control="true"
             data-show-export="true"
             data-click-to-select="true"
             data-toolbar="#toolbar"
             data-sortable="true"
             data-sort-order="asc"
             class="table table-striped table-bordered"
+
             >
 
         <thead>
         <tr>
-            <th data-field="Dataset" data-filter-control="select" data-sortable="true">Dataset</th>
-            <th data-field="Class" data-sortable="true">Vanderbilt's class</th>
-            <th data-field="Correlation" data-sortable="true">Vanderbilt's correlation</th>
-            <th data-field="P-value" data-sortable="true" data-formatter="commFormatter">Vanderbilt's p-value</th>
-            <th data-field="CITBCMST" data-sortable="true">CITBCMST</th>
+            <th data-field="Dataset" data-filter-control="select" data-sortable="true">Dataset<i class="fa fa-fw fa-sort"></i></th>
+            <th data-field="Class" data-sortable="true">Vanderbilt's class<i class="fa fa-fw fa-sort"></i></th>
+            <th data-field="Correlation" data-sortable="true">Vanderbilt's correlation<i class="fa fa-fw fa-sort"></i></th>
+            <th data-field="P-value" data-sortable="true" data-formatter="commFormatter">Vanderbilt's p-value<i class="fa fa-fw fa-sort"></i></th>
+            <th data-field="CITBCMST" data-sortable="true">CITBCMST<i class="fa fa-fw fa-sort"></i></th>
         </tr>
-        </thead>    
+        </thead>
         <tbody>
           <?php foreach ($data as $datum) : ?>
               <tr>
@@ -129,13 +132,13 @@ function linkFormatterGeneset(value)
               echo '<div class="alert alert-warning" id="warningexp">No GSEA has been proceed on this cell line, so there is no data of genes expressions available.</div>';
             } else {
               echo '<h4>There are '.count($genes_actives).' genes across '. count($data) .' replicates.</h4>';
-            } ?> 
+            } ?>
         </blockquote>
 
-        <table id="table" 
+        <table id="table"
           data-toggle="table"
           data-search="true"
-          data-filter-control="true" 
+          data-filter-control="true"
           data-show-export="true"
           data-click-to-select="true"
           data-toolbar="#toolbar"
@@ -153,10 +156,10 @@ function linkFormatterGeneset(value)
         <thead>
         <tr>
             <th data-formatter="linkFormatterGene" data-filter-control="input">Gene Symbol</th>
-            <th data-field="Gene Title" data-filter-control="input" data-sortable="true">Gene Name</th>
-            <th data-field="Score" data-sortable="true">Ranked gene level</th>
+            <th data-field="Gene Title" data-filter-control="input" data-sortable="true">Gene Name<i class="fa fa-fw fa-sort"></i></th>
+            <th data-field="Score" data-sortable="true">Ranked gene level<i class="fa fa-fw fa-sort"></i></th>
         </tr>
-        </thead>    
+        </thead>
         <tbody>
           <?php foreach ($genes_actives as $genes_active) : ?>
               <tr>
@@ -189,14 +192,14 @@ function linkFormatterGeneset(value)
               echo '<li>'.count($gsea_results).' / '.$nbpathways.' gene sets are upregulated in phenotype MCF7.</li>';
               echo '<li>'.count($validation).' gene sets are significant at FDR < 25%.</li>';
               echo '<li>'.count($pval1percent).' gene sets are significantly enriched at nominal pvalue < 1%.  <span class="color-sample valid-GSEA"></span></li>';
-              echo '<li>'.count($pval5percent).' gene sets are significantly enriched at nominal pvalue < 5%.  <span class="color-sample hypothetical-valid-GSEA"></span></li>';              
-          } ?> 
+              echo '<li>'.count($pval5percent).' gene sets are significantly enriched at nominal pvalue < 5%.  <span class="color-sample hypothetical-valid-GSEA"></span></li>';
+          } ?>
         </div>
 
-        <table id="table" 
+        <table id="table"
           data-toggle="table"
           data-search="true"
-          data-filter-control="true" 
+          data-filter-control="true"
           data-show-export="true"
           data-click-to-select="true"
           data-toolbar="#toolbar"
@@ -212,20 +215,20 @@ function linkFormatterGeneset(value)
         >
           <thead>
             <tr>
-                <th data-field="link" data-formatter="linkFormatterGeneset">Pathway's name</th>
-                <th data-field="size" data-sortable="true">Size</th>
-                <th data-field="ES" data-sortable="true">Enrichment Score</th>
-                <th data-field="NES" data-sortable="true">Normalize ES</th>
-                <th data-field="pval" data-sortable="true">Nominale p-value</th>
-                <th data-field="FDR" data-sortable="true">FDR</th>
-                <th data-field="FWER" data-sortable="true">FWER</th>
-                <th data-field="rank_at_max" data-sortable="true">Rank at max</th>
+                <th data-field="link" data-formatter="linkFormatterGeneset" data-sortable="true">Pathway's name<i class="fa fa-fw fa-sort"></th>
+                <th data-field="size" data-sortable="true">Size<i class="fa fa-fw fa-sort"></i></th>
+                <th data-field="ES" data-sortable="true">Enrichment Score<i class="fa fa-fw fa-sort"></i></th>
+                <th data-field="NES" data-sortable="true">Normalize ES<i class="fa fa-fw fa-sort"></i></th>
+                <th data-field="pval" data-sortable="true">Nominale p-value<i class="fa fa-fw fa-sort"></i></th>
+                <th data-field="FDR" data-sortable="true">FDR<i class="fa fa-fw fa-sort"></i></th>
+                <th data-field="FWER" data-sortable="true">FWER<i class="fa fa-fw fa-sort"></i></th>
+                <th data-field="rank_at_max" data-sortable="true">Rank at max<i class="fa fa-fw fa-sort"></i></th>
                 <!-- <th data-field="Leading EDGE" colspan=3 data-sortable="true">Leading EDGE</th> -->
-                <th data-field="Tags" data-sortable="true">Tags</th>
-                <th data-field="List" data-sortable="true">List</th>
-               <th data-field="Signal" data-sortable="true">Signal</th>
+                <th data-field="Tags" data-sortable="true">Tags<i class="fa fa-fw fa-sort"></i></th>
+                <th data-field="List" data-sortable="true">List<i class="fa fa-fw fa-sort"></i></th>
+               <th data-field="Signal" data-sortable="true">Signal<i class="fa fa-fw fa-sort"></i></th>
             </tr>
-          </thead>    
+          </thead>
           <tbody>
             <?php foreach ($gsea_results as $gsea_result) : ?>
                 <tr <?php echo GSEA($gsea_result->NOMpval); ?>>
